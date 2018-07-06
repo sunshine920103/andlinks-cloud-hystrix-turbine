ecf544b132ce59baab41b4d1f766d0e4c41c4427
chencong <1085576236@qq.com>
2018-07-06 14:39:24 +0800

    first commit

---
+++ src/main/java/com/andlinks/cloud/andlinkscloudhystrixturbine/AndlinksCloudHystrixTurbineApplication.java
@@ -0,0 +1,12 @@
+package com.andlinks.cloud.andlinkscloudhystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndlinksCloudHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndlinksCloudHystrixTurbineApplication.class, args);
+	}
+}
